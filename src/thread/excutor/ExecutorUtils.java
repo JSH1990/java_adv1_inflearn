d5a5b1b5bb93cfcf2c8dee8a1bc8518282a139b9
DESKTOP-SN9D63J\JSH <tmdgus21@naver.com>
2024-09-11 17:47:06 +0900

    //ExecutorUtils

---
+++ src/thread/excutor/ExecutorUtils.java
@@ -0,0 +1,34 @@
+package thread.excutor;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import static util.MyLogger.log;
+
+public class ExecutorUtils {
+
+    public static void printState(ExecutorService executorService){
+        if(executorService instanceof ThreadPoolExecutor poolExecutor){
+            int pool = poolExecutor.getPoolSize();
+            int active = poolExecutor.getActiveCount();
+            int queuedTasks = poolExecutor.getQueue().size();
+            long completedTasks = poolExecutor.getCompletedTaskCount();
+            log("[pool=" + pool + ", active=" + active + ", queuedTasks=" + queuedTasks + ", completedTasks=" + completedTasks + "]");
+        }else {
+            log(executorService);
+        }
+    }
+
+    //추가
+    public static void printState(ExecutorService executorService, String taskName){
+        if(executorService instanceof ThreadPoolExecutor poolExecutor){
+            int pool = poolExecutor.getPoolSize();
+            int active = poolExecutor.getActiveCount();
+            int queuedTasks = poolExecutor.getQueue().size();
+            long completedTasks = poolExecutor.getCompletedTaskCount();
+            log(taskName + " -> [pool=" + pool + ", active=" + active + ", queuedTasks=" + queuedTasks + ", completedTasks=" + completedTasks + "]");
+        }else {
+            log(executorService);
+        }
+    }
+}
---
+++ src/thread/excutor/poolsize/PoolSizeMainV1.java
@@ -0,0 +1,11 @@
+package thread.excutor.poolsize;
+
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class PoolSizeMainV1 {
+    public static void main(String[] args) {
+        ArrayBlockingQueue<Object> workQueue = new ArrayBlockingQueue<>(2);
+        
+
+    }
+}
